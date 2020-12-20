976fb4192737dc28173f223b8f4eec8c6e7f5137
Shobhit Yadav <shobhit999000@gmail.com>
2020-12-20 21:30:44 +0530

    Burger getting better

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import classes from './Burger.css';
+import BurgerIngredients from './BurgerIngredients/BurgerIngredients';
 import BurgerIngredient from './BurgerIngredients/BurgerIngredients'
 
 const burger =(props) => {
+    const transformedIngredients=Object.keys(props.ingredients).map(igkey=> {
+        return [...Array(props.ingredients[igkey])].
+        map((_, i) => {return <BurgerIngredient key={igkey+i} type={igkey} />;
+    });
+    });
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"></BurgerIngredient>
-            <BurgerIngredient type="cheese"></BurgerIngredient>
-            <BurgerIngredient type="meat"></BurgerIngredient>
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom"></BurgerIngredient>
         </div>
     );
