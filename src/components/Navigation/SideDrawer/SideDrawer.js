98de8e571c2ffd00456831ae199402c71583bc09
Shobhit Yadav <shobhit999000@gmail.com>
2020-12-24 02:19:32 +0530

    backdrop added

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,15 +2,20 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
+import Aux from '../../../hoc/Auxillary';
+import BackDrop from '../../UI/Backdrop/Backdrop';
 
 const sideDrawer = (props) => {
     return(
+        <Aux>
+            <BackDrop show/>
         <div className={classes.SideDrawer}>
             <Logo height="11%"/>
             <nav>
                 <NavigationItems />
             </nav>
         </div>
+        </Aux>
     );
 };
 
