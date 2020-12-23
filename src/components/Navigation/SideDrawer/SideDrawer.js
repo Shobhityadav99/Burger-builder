468ed2986aa6e77834722fa0d7180476baf8b97b
Shobhit Yadav <shobhit999000@gmail.com>
2020-12-23 21:40:56 +0530

    Side drawer added

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './SideDrawer.css';
 
 const sideDrawer = (props) => {
     return(
