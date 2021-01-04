489c9c44580e381973cf6a87bb01bbb351bba44b
Shobhit Yadav <shobhit999000@gmail.com>
2021-01-04 20:14:14 +0530

    Ingredients are passed ahead now

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -112,7 +112,15 @@ class BurgerBuilder extends Component {
         //     error => {this.setState({loading: false, purchasing: false})}
         // );
         // // alert('You can continue!');
-        this.props.history.push('/checkout');
+        const queryParams =[];
+        for(let i in this.state.ingredients){
+            queryParams.push(encodeURIComponent(i)+ "=" + encodeURIComponent(this.state.ingredients[i]));
+        }
+        const queryString=queryParams.join('&');
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?'+queryString,
+        });
     }
 
 
