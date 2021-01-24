6e3ea7a34f3bb11f2f83585a3869cf95bb250594
Shobhit Yadav <shobhit999000@gmail.com>
2021-01-24 14:57:01 +0530

    minor issues fixed

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     let routes=(
       <Switch>
           <Route path="/auth" component={Auth} />
-          <Route path="/" component={BurgerBuilder}/>
+          <Route path="/" exact component={BurgerBuilder}/>
           <Redirect to = "/" />
       </Switch>
     );
@@ -29,8 +29,10 @@ class App extends Component {
         <Switch>
           <Route path="/orders" component={Orders}/>
           <Route path="/checkout" component={Checkout}/>
+          <Route path="/auth" component={Auth} />
           <Route path="/logout" component={Logout} />
-          <Route path="/" component={BurgerBuilder}/>
+          <Route path="/" exact component={BurgerBuilder}/>
+          <Redirect to = "/" />
       </Switch>
       );
     }
--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -20,8 +20,8 @@ class OrderSummary extends Component {
             </ul>
             <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
             <p>Continue to checkout!!</p>
-            <Button btnType="Danger" clicked={this.props.purchaseCanceled}>CANCEL</Button>
-            <Button btnType="Success" clicked={this.props.purchaseContinue}>CONTINUE</Button>
+            <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+            <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
         </Aux>
         );
     }
