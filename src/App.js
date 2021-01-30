7f553a57e4ff4dba72efdcf2ca774c1eee52697c
Shobhit Yadav <shobhit999000@gmail.com>
2021-01-30 22:02:38 +0530

    Layout component done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect , Suspense } from 'react';
 import { Route, Switch , withRouter , Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import Layout from './components/Layout/Layout';
+import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Logout from './containers/Auth/Logout/Logout';
 import * as actions from './store/actions/index'; 
